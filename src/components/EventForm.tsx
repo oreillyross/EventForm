db2c6bce1b5ec1b012cc81c05cdc1a1b252a0b76
Faktor 10 <oreillyross@gmail.com>
2020-09-01 16:29:50 +0200

    add bootstrap

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,14 +1,43 @@
 import React from "react";
-import TextField from "./TextField";
+import { Formik } from "formik";
+import { Button, Form } from 'react-bootstrap'
 
-type FormData = {
-  url?: string;
-  text?: string;
-  tags?: string[];
-};
-
-const EventForm: React.FC = () => {
-  return (<div><TextField text='Something'/></div>);
+interface Props {
+  formData?: {
+    url?: string;
+    text?: string;
+    tags?: string[];
+  };
 }
 
-export default EventForm
+const EventForm: React.FC<Props> = ({ formData }) => {
+  return (
+    <div>
+      <Formik
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        initialValues={{ url: "" }}
+      >
+        {({ values, handleSubmit, handleChange }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <Form.Group controlId='formUrlId'>
+              <Form.Label>Url</Form.Label>
+              <Form.Control
+                type='text'
+                onChange={handleChange}
+                name="url"
+                value={values.url}
+              />
+              </Form.Group>
+              <Button variant='primary' type="submit">Add event</Button>
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+
+export default EventForm;
