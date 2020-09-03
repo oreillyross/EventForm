5dc41cc1b05ef57ed043ca2178da66524d4941f3
Faktor 10 <oreillyross@gmail.com>
2020-09-03 17:01:31 +0200

    stuff

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Formik } from "formik";
+import "./EventForm.css";
+import { Typeahead } from "react-bootstrap-typeahead";
 
 interface Props {
   formData?: {
@@ -16,22 +18,38 @@ const EventForm: React.FC<Props> = ({ formData }) => {
         onSubmit={(values) => {
           console.log(values);
         }}
-        initialValues={{ url: "" }}
+        initialValues={{ url: "", text: "" }}
       >
         {({ values, handleSubmit, handleChange }) => {
           return (
             <form onSubmit={handleSubmit}>
               <label>Url</label>
               <input
+                className="inputs"
                 type="text"
+                placeholder="type url to article here..."
                 onChange={handleChange}
                 name="url"
                 value={values.url}
               />
+              <label>Text</label>
+              <textarea
+                className="inputs"
+                placeholder="copy quoted text here..."
+                onChange={handleChange}
+                name="text"
+                value={values.text}
+              />
+              <Typeahead
+                allowNew
+                id="custom-selections-example"
+                multiple
+                newSelectionPrefix="Add a new item: "
+                options={[]}
+                placeholder="Type tags..."
+              />
 
-              <button type="submit">
-                Add event
-              </button>
+              <button type="submit">Add event</button>
             </form>
           );
         }}
